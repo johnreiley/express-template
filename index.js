7e03fd6da6e45f22f831475f65437e4a6ad411ff
John Reiley <supermormonbrother@gmail.com>
2021-02-15 13:46:58 -0700

    made even more barebones

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, '../public')))
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.listen(port, () => {
